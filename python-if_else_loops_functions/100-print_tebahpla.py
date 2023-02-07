156d934d2e514c50f32c4ef952f50e7cac98d049
Cynthia <c.umugwanez@alustudent.com>
2023-02-07 05:51:13 -0800

    kkkk

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,4 @@ for alpha in range(ord('z'), ord('a')-1, -1):
     if alpha % 2 != 0:
         print("{}".format(chr(alpha-32)), end="")
     else:
-        print(f"{chr(alpha)}", end="")                                    
+        print(f"{chr(alpha)}", end="")                                  
