b17f0e51d6801a175f8a660df92e8c584fa0c3ba
Cynthia <c.umugwanez@alustudent.com>
2023-02-28 13:00:47 -0800

    qqqq

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    length = len(my_list)
+    if length == 0:
+        return None
+    big = my_list[0]
+    for x in my_list:
+        if x > big:
+            big =
+    return big
