fbfcf22cb05ae5ae8bb46e68f0fdb01aa08b7b2c
Cynthia <c.umugwanez@alustudent.com>
2023-03-01 06:00:22 -0800

    rrrr

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-max_integer = __import__('9-max_integer').
-my_list = [1, 90, 2, 13, 34, 5, -13, 3]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))
+def max_integer(my_list=[]):
+    length = len(my_list)
+    if length == 0:
+        return None
+    big = my_list[0]
+    for x in my_list:
+        if x > big:
+            big = x
+    return big
